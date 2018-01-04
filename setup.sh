9c9a6ab2be1759ffb107d1704c1f3873e3b32dc7
Hiroki Uchida <nikuyoshi@gmail.com>
2018-01-04 18:32:57 +0900

    Fixed setting files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
-# This directory is supporsed to be in $HOME as .dotfiles
-DOTFILES="$HOME"/.dotfiles
-cd "$HOME"
+#!/usr/bin/env bash
+CURRENTDIR=`pwd`
+VIMSETTINGDIR=${HOME}/.vim
+VIMCOLORDIR=${VIMSETTINGDIR}/colors
 
-# set editor configuration files
-ln -s "$DOTFILES"/_vimrc _vimrc
+if [ ! -f ${HOME}/_vimrc ]; then
+  ln -s ${CURRENTDIR}/_vimrc ${HOME}/_vimrc
+fi
+if [ ! -f ${HOME}/_gvimrc ]; then
+  ln -s ${CURRENTDIR}/_gvimrc ${HOME}/_gvimrc
+fi
+if [ ! -d ${VIMCOLORDIR} ]; then
+  mkdir -p ${VIMCOLORDIR}
+fi
+if [ ! -f ${VIMCOLORDIR} ]; then
+  ln -s ${CURRENTDIR}/molokai/colors/molokai.vim ${VIMCOLORDIR}
+fi
