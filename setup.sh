2b8cf27b69d5ab5fe74b8fa437d631550ac86601
Hiroki Uchida <nikuyoshi@gmail.com>
2019-02-13 10:13:24 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,28 @@ CURRENTDIR=`pwd`
 VIMSETTINGDIR=${HOME}/.vim
 VIMCOLORDIR=${VIMSETTINGDIR}/colors
 
-if [ ! -f ${HOME}/_vimrc ]; then
-  ln -s ${CURRENTDIR}/_vimrc ${HOME}/_vimrc
-fi
-if [ ! -f ${HOME}/_gvimrc ]; then
-  ln -s ${CURRENTDIR}/_gvimrc ${HOME}/_gvimrc
-fi
+declare -a dotfiles=(
+  '_vimrc'
+  '_gvimrc'
+  '.pecorc'
+  '.gitignore'
+  '.zshrc'
+)
+
+for dotfile in "${dotfiles[@]}"
+do
+  if [ ! -f ${HOME}/"${dotfile}" ]; then
+    ln -sf ${CURRENTDIR}/"${dotfile}" ${HOME}/"${dotfile}"
+  fi
+done
+echo "finished linking ${#dotfiles[@]} files"
+
 if [ ! -d ${VIMCOLORDIR} ]; then
   mkdir -p ${VIMCOLORDIR}
 fi
+echo "finished creating ${VIMCOLORDIR}"
+
 if [ ! -f ${VIMCOLORDIR} ]; then
-  ln -s ${CURRENTDIR}/molokai/colors/molokai.vim ${VIMCOLORDIR}
+  ln -sf ${CURRENTDIR}/molokai/colors/molokai.vim ${VIMCOLORDIR}
 fi
+echo "finished linking molokai.vim"
