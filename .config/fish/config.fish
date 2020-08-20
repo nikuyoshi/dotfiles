24d264b97218667f8705c75a4c460e815c348a34
Hiroki Uchida <nikuyoshi@gmail.com>
2020-08-20 22:55:42 +0900

    Modify config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 set fish_plugins theme peco
-alias vim="mvim -v"
+set fish_theme robbyrussell
+# alias vim="mvim -v"
+
 
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
