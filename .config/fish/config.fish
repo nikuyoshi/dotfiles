f71bd6f3c79d2254121503ec4432585a6f32e1bc
Hiroki Uchida <nikuyoshi@gmail.com>
2020-08-20 23:25:05 +0900

    Modify config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,13 @@
 set fish_plugins theme peco
 set fish_theme robbyrussell
-# alias vim="mvim -v"
-
 
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
 end
+
+# macOS settings
+# alias vim="mvim -v"
+
+# Ubuntu settings
+command /usr/bin/keychain $HOME/.ssh/id_rsa
+eval (keychain --eval --agents ssh id_rsa | source)
