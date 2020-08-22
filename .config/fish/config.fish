29339f139b28b94d54c339501df839d4aff87d7b
Hiroki Uchida <nikuyoshi@gmail.com>
2020-08-22 20:57:34 +0900

    Add alias to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,8 @@ function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
 end
 
+alias start=/mnt/c/Windows/explorer.exe
+
 # macOS settings
 # alias vim="mvim -v"
 
