830fac77d41c7427380f575d26ca18f3b42900be
Hiroki Uchida <nikuyoshi@gmail.com>
2020-12-29 12:16:25 +0900

    Modify config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,14 +5,24 @@ function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
 end
 
-alias start=/mnt/c/Windows/explorer.exe
+# Determine OS platform
+set UNAME (uname | tr "[:upper:]" "[:lower:]")
+# If Linux, try to determine specific distribution
+if [ "$UNAME" = "linux" ]
+   set DISTRO (awk -F= '/^NAME/{print $2}' /etc/os-release | sed s/'\"'//g)
+end
+set -e UNAME
+export $DISTRO
 
-# macOS settings
-# alias vim="mvim -v"
-
-# Ubuntu settings
-command /usr/bin/keychain $HOME/.ssh/id_rsa
-eval (keychain --eval --agents ssh id_rsa | source)
+switch $DISTRO
+  case Darwin
+    alias vim="mvim -v"
+  case Ubuntu
+     command /usr/bin/keychain $HOME/.ssh/id_rsa
+     eval (keychain --eval --agents ssh id_rsa | source)
+     alias start=/mnt/c/Windows/explorer.exe
+  case Amazon Linux
+end
 
 set PATH ~/.local/bin $PATH
 
