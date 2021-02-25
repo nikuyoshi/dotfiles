735ea0d8f550343d0074308f8ffac7ee6c8af748
Hiroki Uchida <nikuyoshi@gmail.com>
2021-02-26 01:38:38 +0900

    Add homebrew settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,6 +25,14 @@ end
 
 set PATH ~/.local/bin $PATH
 
+# Homebrew
+set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+set -q PATH; or set PATH ''; set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH;
+set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
+set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
+
 # cfn-guard
 # set PATH ~/dev/readonly/cloudformation-guard/bin $PATH
 
