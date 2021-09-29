1f393fa686831d6f5a172de4481895ce6781f2ac
Hiroki Uchida <nikuyoshi@gmail.com>
2021-09-30 01:40:40 +0900

    Modify config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,3 +38,21 @@ set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbre
 
 # Go
 # set PATH /usr/local/go/bin $PATH
+
+# Jump
+function __jump_add --on-variable PWD
+  status --is-command-substitution; and return
+  jump chdir
+end
+
+function __jump_hint
+  set -l term (string replace -r '^j ' '' -- (commandline -cp))
+  jump hint "$term"
+end
+
+function j
+  set -l dir (jump cd "$argv")
+  test -d "$dir"; and cd "$dir"
+end
+
+complete --command j --exclusive --arguments '(__jump_hint)'
