781a3120f3252b201a8c5c8cea7a72ca69cfa8ae
Hiroki Uchida <nikuyoshi@gmail.com>
2022-05-08 21:05:44 +0900

    fix: :bug: Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+
+set -eu
+
 DOTFILESDIR=${HOME}/dev/workspace/dotfiles
 VIMSETTINGDIR=${HOME}/.vim
 VIMCOLORDIR=${VIMSETTINGDIR}/colors
+
+declare -a dotfolders=(
+  '.config/fish/'
+  '.config/omf/'
+  '.vim/colors'
+)
 
 declare -a dotfiles=(
   '_vimrc'
@@ -14,20 +23,32 @@ declare -a dotfiles=(
   '.config/omf/theme'
 )
 
+for dotfolder in "${dotfolders[@]}"
+do
+  if [ -d ${HOME}/"${dotfolder}" ]; then
+    echo Skipped: ${HOME}/"${dotfolder}" exist
+  else
+    echo Create folder: ${HOME}/"${dotfolder}"
+    mkdir -p ${HOME}/"${dotfolder}"
+  fi
+done
+echo "Finished creating ${#dotfolders[@]} folders"
+
 for dotfile in "${dotfiles[@]}"
 do
-  if [ ! -f ${HOME}/"${dotfile}" ]; then
+  if [ -f ${HOME}/"${dotfile}" ]; then
+    echo Skipped: ${HOME}/"${dotfile}" exists
+  else
+    echo Create symbolic link: From: ${DOTFILESDIR}/"${dotfile}", To: ${HOME}/"${dotfile}"
     ln -sf ${DOTFILESDIR}/"${dotfile}" ${HOME}/"${dotfile}"
   fi
 done
-echo "finished linking ${#dotfiles[@]} files"
+echo "Finished linking ${#dotfiles[@]} files"
 
-if [ ! -d ${VIMCOLORDIR} ]; then
-  mkdir -p ${VIMCOLORDIR}
-fi
-echo "finished creating ${VIMCOLORDIR}"
-
-if [ ! -f ${VIMCOLORDIR} ]; then
+if [ -f ${VIMCOLORDIR}/molokai.vim ]; then
+  echo Skipped: ${VIMCOLORDIR}/molokai.vim exists
+else 
+  echo Create symbolic link: From: ${DOTFILESDIR}/molokai/colors/molokai.vim, To: ${VIMCOLORDIR}
   ln -sf ${DOTFILESDIR}/molokai/colors/molokai.vim ${VIMCOLORDIR}
+  echo "finished linking molokai.vim"
 fi
-echo "finished linking molokai.vim"
